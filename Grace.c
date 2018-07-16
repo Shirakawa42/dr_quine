d7c122af6a463b1b15b47d1b92649776a1ebc528
shirakawa <lvasseur@student.42.fr>
2018-07-16 16:15:21 +0200

    Grace.c

--- Grace.c
+++ Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //com
 #define H
-#define A "#include <stdio.h>%c//com%c#define H%c#define A %c%s%c%c#define MAIN int main(){printf(A,10,10,10,34,A,34,10,10,10);}%cMAIN%c"
-#define MAIN int main(){FILE *f = fopen("Grace_kid.c", "w"); printf(A,10,10,10,34,A,34,10,10,10);}
+#define A "#include <stdio.h>%c//com%c#define H%c#define A %c%s%c%c#define MAIN int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,A,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}%cMAIN%c"
+#define MAIN int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,A,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}
 MAIN
