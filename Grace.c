8cd0b0ef3b7692edfed685a8ab70bd7d60fba998
shirakawa <lvasseur@student.42.fr>
2018-07-16 16:29:22 +0200

    format com & FT(x)

--- Grace.c
+++ Grace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//com
+/*
+    printing source
+*/
 #define H
-#define A "#include <stdio.h>%c//com%c#define H%c#define A %c%s%c%c#define MAIN int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,A,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}%cMAIN%c"
-#define MAIN int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,A,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}
-MAIN
+#define A "#include <stdio.h>%c/*%c    printing source%c*/%c#define H%c#define A %c%s%c%c#define FT(x) int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,A,10,10,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}%cFT(0)%c"
+#define FT(x) int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,A,10,10,10,10,10,34,A,34,10,34,34,34,34,10,10);fclose(f);}
+FT(0)
